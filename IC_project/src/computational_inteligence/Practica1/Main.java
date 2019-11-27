5a09339675ac8af377f5b11859c27070e10939a3
Fernando Roldán Zafra <Doskoy93@gmail.com>
2019-11-27 20:40:26 +0100

    Se ha cambiado el nombre de la clase trainer, escribiendo la primera letra en mayuscula. Se han añadido los atributos, el constructor y se han añadido las cabeceras de los metodos que faltan por implementar. Closes #1 Closes#2

--- IC_project/src/computational_inteligence/Practica1/Main.java
+++ IC_project/src/computational_inteligence/Practica1/Main.java
@@ -5,13 +5,41 @@
  */
 package computational_inteligence.Practica1;
 
+import static computational_inteligence.Practica1.MNISTDatabase.normalize;
+import static computational_inteligence.Practica1.MNISTDatabase.readImages;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Fernando Roldán Zafra
 */
 
 public class Main {
-//    public static void main(String[] args) {
-//        // TODO code application logic here
-//    }
+    public static void main(String[] args) throws IOException {
+        String url = "D:\\Documentos\\NetBeansProjects\\Inteligencia_computacional\\IC_project\\data\\mnist\\";
+        MNISTDatabase.downloadMNIST(url);
+        int images_train[][][];
+        int images_test[][][];
+        images_train = readImages("data\\mnist\\train-images-idx3-ubyte.gz");
+        images_test = readImages("data\\mnist\\t10k-images-idx3-ubyte.gz");
+        
+        ArrayList<float[][]> train_datas = new ArrayList();
+        ArrayList<float[][]> test_datas = new ArrayList();
+        float data_train[][] = null;
+        float data_test[][] = null;
+        for(int i = 0; i <= images_train.length; i++){
+            data_train = MNISTDatabase.normalize(images_train[i]);  
+            data_test = MNISTDatabase.normalize(images_train[i]);
+        }
+        
+        int labels_train[];
+        int labels_test[];
+		labels_train = MNISTDatabase.readLabels("data\\mnist\\train-labels-idx1-ubyte.gz");
+		labels_test = MNISTDatabase.readLabels("data\\mnist\\t10k-labels-idx1-ubyte.gz");
+        
+        
+        
+    
+    }
 }
